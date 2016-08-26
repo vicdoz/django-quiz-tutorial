235c9ce452a9afc7fa2685577fcd1ef836e63aa5
vahuir <v.ahuir@gmail.com>
2016-08-26 14:00:47 +0200

    Sort order fields admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+      fields = ['pub_date', 'question_text']
+
+admin.site.register(Question,QuestionAdmin)
